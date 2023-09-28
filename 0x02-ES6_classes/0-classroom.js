5fb44969d0ff95fc7c9deaf3c10ce5b19cd98d06
KofiFloki <jeremiah.martey@yahoo.com>
2023-09-28 14:41:42 -0700

    Task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -3,11 +3,11 @@
  */
 export default class ClassRoom {
   /**
-   * Creates a new @see {@link ClassRoom}.
-   * @param {Number} maxStudentsSize - The maximum number of
+   * Creates a new {@link ClassRoom}.
+   * @param {number} maxStudentsSize - The maximum number of
    * students in the class.
    */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
- }
+}
