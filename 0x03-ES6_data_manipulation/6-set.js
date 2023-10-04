496ae3fe5c0b3047aa76d75af4281cd8dc5248bd
KofiFloki <jeremiah.martey@yahoo.com>
2023-10-03 22:42:13 -0700

    Set data structure

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * This creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Jeremiah Martey <https://github.com/KofiFloki>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
