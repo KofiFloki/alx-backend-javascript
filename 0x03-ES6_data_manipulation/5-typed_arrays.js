dcb639a063ac468e6493c41a2a0242d0e1c1eaf7
KofiFloki <jeremiah.martey@yahoo.com>
2023-10-03 22:40:53 -0700

    Typed arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,5 @@
 /**
- * Creates a buffer array with a given position set to a given value.
+ * This creates a buffer array with a given position set to a given value.
  * @param {Number} length - The length of the buffer.
  * @param {Number} position - The position to modify.
  * @param {Number} value - The value to be stored in the position.
