b6b34d3466e5a5905f85a06c8dda619e885195fb
KofiFloki <jeremiah.martey@yahoo.com>
2023-10-03 22:34:03 -0700

    Reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,19 @@
+/**
+ * Retrieves students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author Jeremiah Marteyx <https://github.com/KofiFloki>
+ * @returns
+ */
+export default function getStudentIdsSum(students) {
+  if (students instanceof Array) {
+    return students.reduce(
+      (prevStudent, curStudent) => prevStudent.id || prevStudent + curStudent.id,
+      0,
+    );
+  }
+  return 0;
+}
