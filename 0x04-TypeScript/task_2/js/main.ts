d913edb6167e3c2f522791b5e020df17fb29531a
KofiFloki <Jeremiah.martey@yahoo.com>
2023-10-06 22:54:12 +0000

    Export types, interfaces, and classes

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,87 +1,68 @@
-export interface Student {
+interface Teacher {
+    readonly  firstName: string;
+    readonly  lastName: string;
+    fullTimeEmployee: boolean;
+    location: string;
+    yearsOfExperience?: number;
+    [propName: string]: any;
+  }
+  
+  const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
+  
+  
+  interface Directors extends Teacher {
+    numberOfReports: number;
+  }
+  
+  const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  
+  console.log(director1);
+  
+  interface PrintTeacherFunction {
+    (firstName: string, lastName: string): string;
+  }
+  
+  const printTeacher: PrintTeacherFunction = function(firstName, lastName) {
+    return `${firstName.charAt(0)}.${lastName}`;
+  };
+  
+  interface StudentClassInterface  {
+    workOnHomework(): string;
+    displayName(): string;
+  }
+  
+  interface StudentConstructor {
+    new(firstName: string, lastName: string): StudentClassInterface;
+  }
+  
+  class StudentClass implements StudentClassInterface {
     firstName: string;
     lastName: string;
-    age: number;
-    location: string;
+  
+    constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+  
+    workOnHomework(): string {
+      return 'Currently working';
+    }
+  
+    displayName(): string {
+      return this.firstName;
+    }
   }
-  const studentA: Student = {
-    firstName: "Harry",
-    lastName: "Yoon",
-    age: 22,
-    location: "Seoul, South Korea",
-  };
-  const studentB: Student = {
-    firstName: "Anjali",
-    lastName: "Prajapati",
-    age: 22,
-    location: "Lodz, Poland",
-  };
-  
-  const studentsList: Array<Student> = [
-    studentA,
-    studentB,
-  ];
-  const styleSheet = `
-    html {
-      margin: 0;
-      height: 100%;
-    }
-    body {
-      box-sizing: border-box;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 80%;
-      margin: 10%;
-    }
-    table {
-      border-collapse: collapse;
-    }
-    thead {
-      font-weight: bold;
-    }
-    td {
-      padding: 10px;
-      border: 1px solid gray;
-      cursor: pointer;
-    }
-    td:hover {
-      background: gainsboro;
-    }
-  
-    td:nth-child(1) {
-      text-align: center;
-    }
-  `;
-  
-  /**
-   * Displays information about students in a table.
-   * @param students The list of students to display.
-   * @author Bezaleel Olakunori <https://github.com/B3zaleel>
-   */
-  export const displayStudents = (students: Array<Student>): void => {
-    const table = document.createElement('table');
-    const tableHead = document.createElement('thead');
-    const headRow = document.createElement('tr');
-    const tableBody = document.createElement('tbody');
-    headRow.insertAdjacentHTML('beforeend', '<td>FirstName</td');
-    headRow.insertAdjacentHTML('beforeend', '<td>Location</td');
-    tableHead.insertAdjacentElement('beforeend', headRow);
-  
-    for (const student of students) {
-      const bodyRow = document.createElement('tr');
-      bodyRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
-      bodyRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
-      tableBody.insertAdjacentElement('beforeend', bodyRow);
-    }
-  
-    table.insertAdjacentElement('beforeend', tableHead);
-    table.insertAdjacentElement('beforeend', tableBody);
-    document.body.insertAdjacentElement('beforeend', table);
-  };
-  
-  displayStudents(studentsList);
-  const styleSheetElement = document.createElement('style');
-  styleSheetElement.innerHTML = styleSheet;
-  document.head.insertAdjacentElement('beforeend', styleSheetElement);
-  document.title = 'Task 0';
